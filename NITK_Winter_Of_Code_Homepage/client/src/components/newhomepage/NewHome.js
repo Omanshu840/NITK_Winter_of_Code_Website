b9ddce5e819109ca29b23c2a4b9db18bfde7312c
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-10-02 11:54:56 +0530

    Adding Timeline

--- NITK_Winter_Of_Code_Homepage/client/src/components/newhomepage/NewHome.js
+++ NITK_Winter_Of_Code_Homepage/client/src/components/newhomepage/NewHome.js
@@ -1,6 +1,6 @@
 import { reach } from "yup";
 import React from "react";
-import { Jumbotron, Container, Row } from 'reactstrap';
+import { Jumbotron, Container, Row, Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import "./newhome.css";
 
 const NewHome = () => {
@@ -13,10 +13,10 @@ const NewHome = () => {
                 </div>
             </Jumbotron>
 
-            <Container className="section">
+            <Container className="intro padding">
                 <Row className="text-center">
                     <div className="col-12">
-                        <h1 className="display-4">
+                        <h1 className="display-3">
                             KickStart Open Source
                         </h1>
                     </div>
@@ -24,40 +24,55 @@ const NewHome = () => {
 
                 <Row className="text-center">
                     <div className="col-12">
-                        <p className="lead">
+                        <p>
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                         </p>
                     </div>
 
                 </Row>
-
-                <hr className="my-4" />
             </Container>
 
-            <Container className="section">
+            <Container className="section-2 padding">
                 <Row className="text-center">
                     <div className="col-sm-12 col-md-4">
-                        <h3>Student</h3>
-                        <p>Students contact the open source organizations they want to work with and write up a project proposal for the summer.</p>
+                        <Card className="py-3 my-3">
+                            <CardBody>
+                                <CardTitle className="card-heading"><h2>Students</h2></CardTitle>
+                                <CardText>Students contact the open source organizations they want to work with and write up a project proposal for the summer.</CardText>
+                            </CardBody>
+                        </Card>
                     </div>
 
                     <div className="col-sm-12 col-md-4">
-                        <h3>Organizations</h3>
-                        <p>Open source projects apply to be mentor organizations. Once accepted, organizations discuss possible ideas with students. </p>
+                        <Card className="py-3 my-3">
+                            <CardBody>
+                                <CardTitle className="card-heading"><h2>Organizations</h2></CardTitle>
+                                <CardText>Open source projects apply to be mentor organizations. Once accepted, organizations discuss possible ideas with students.</CardText>
+                            </CardBody>
+                        </Card>
                     </div>
 
                     <div className="col-sm-12 col-md-4">
-                        <h3>Mentors</h3>
-                        <p>Existing contributors with the organizations can choose to mentor a student project. </p>
+                        <Card className="py-3 my-3">
+                            <CardBody>
+                                <CardTitle className="card-heading"><h2>Mentors</h2></CardTitle>
+                                <CardText>Existing contributors with the organizations can choose to mentor a student project. Mentor interaction is a vital part of the program.</CardText>
+                            </CardBody>
+                        </Card>
                     </div>
                 </Row>
-                <hr></hr>
             </Container>
 
-            <Container className="section">
+            <Container className="section-3 padding">
                 <Row>
                     <div className="col-12 col-md-6">
-                        <h1>How does it work?</h1>
+                        <img src={require('./img/img2.jpg')} alt="img" className="img-fluid" />
+                        <div className="caption text-white">
+                            <h1 className="display-2">How Does <br />it Work?</h1>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6 content pt-5">
                         <p>
                             Open source projects apply to be mentor organizations. Once accepted, organizations discuss possible ideas with students and then decide on the proposals they wish to mentor for the summer.
                         </p>
@@ -68,29 +83,58 @@ const NewHome = () => {
                             Existing contributors with the organizations can choose to mentor a student project. Mentors and students work together to determine appropriate milestones and requirements for the summer.
                         </p>
                     </div>
-
-                    <div className="col-lg-6">
-                        <img src={require('./img/img2.jpg')} alt="img" className="img-fluid" />
-                    </div>
                 </Row>
             </Container>
 
-            <figure>
-                <div className="fixed-wrap">
-                    <div id="fixed">
-
-                    </div>
-                </div>
-            </figure>
-
-            <Container>
+            <Container className="padding">
                 <Row>
                     <div className="col-12">
-                        <h1>Eligibility</h1>
+                        <h1 className="display-2 my-5">Eligibility</h1>
+                        <p className="my-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
                     </div>
                 </Row>
             </Container>
-        </div>
+
+            <Container className="padding">
+                <div className="col-12">
+                    <h1 className="display-2 my-5">Timeline</h1>
+                </div>
+                <div className="Timeline">
+                    <ul>
+                        <li className="left">
+                            <div className="content">
+                                <h2>heading</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            </div>
+                            <div className="time">
+                                1 December 2020
+                            </div>
+                        </li>
+
+                        <li className="right">
+                            <div className="content">
+                                <h2>heading</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            </div>
+                            <div className="time">
+                                10 December 2020
+                        </div>
+                        </li>
+
+                        <li className="left">
+                            <div className="content">
+                                <h2>heading</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            </div>
+                            <div className="time">
+                                1 December 2020
+                        </div>
+                        </li>
+                        <div style={{ clear: "both" }}></div>
+                    </ul>
+                </div>
+            </Container>
+        </div >
     )
 }
 
